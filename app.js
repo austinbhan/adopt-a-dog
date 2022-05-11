4cb16521e1e1b772d91cd76b4a5a0dc32f39a0a2
austinbhan <austinbhan@outlook.com>
2022-05-11 12:53:40 -0700

    Loaded dogs onto front page, with little understanding why/how

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 import { getDogs } from './fetch-utils.js';
 import { renderDogCard } from './render-utils.js';
 
-const dogListContainer = document.getElementById('dog-list-container');
-
 // on load
+
+async function loadDogs() {
+    const dogs = await getDogs();
+    console.log(dogs);
+    const main = document.getElementById('dog-list-container');
+
+
+    for (let dog of dogs) {
+        const dogDiv = renderDogCard(dog);
+        main.append(dogDiv);
+    }
+}
 // fetch all dogs
 // render and append all dog cards to the container
+
+loadDogs();
