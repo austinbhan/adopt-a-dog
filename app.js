191eca2c85e2b74abdb298bdf05fa21f99d1f381
austinbhan <austinbhan@outlook.com>
2022-05-11 15:00:48 -0700

    Working, but little understanding how

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 import { getDogs } from './fetch-utils.js';
 import { renderDogCard } from './render-utils.js';
 
-const dogDetailContainer = document.getElementById('dog-detail-container'); // Will I need this later??
+const dogListContainer = document.getElementById('dog-list-container'); // Will I need this later??
 
 // on load
 
 async function loadDogs() {
     const dogs = await getDogs();
     console.log(dogs);
-    const main = document.getElementById('dog-list-container');
 
 
     for (let dog of dogs) {
         const dogDiv = renderDogCard(dog);
-        main.append(dogDiv);
+        dogListContainer.append(dogDiv);
     }
 }
 // fetch all dogs
